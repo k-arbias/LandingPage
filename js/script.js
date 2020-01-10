a1a2415e8ae39227d93316f0a1511cf9563c4a3f
Arbias <52889109+k-arbias@users.noreply.github.com>
2020-01-10 21:08:03 +0100

    Added isotope plugin

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ element.forEach(function(){
    })
 });
 
+ $(document).ready(function(){
+
+   $(".items").isotope({
+      filter: '*',
+      animationOptions: {
+         duration: 1500,
+         easing: 'linear',
+         queue: false
+      }
+   });
+
+   $("#filters a").click(function(){
+      $("#filters .current").removeClass("current");
+      $(this).addClass("current");
+
+      var selector = $(this).attr("data-filter");
+
+      $(".items").isotope({
+         filter: selector,
+         animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+         }
+      });
+      return false;
+   })
+
+ });
